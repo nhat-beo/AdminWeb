3554ae35b3ed21283b82395d3172492dcc0b6cd1
DESKTOP-88FAOV3\EdsoLabs <>
2021-11-29 21:41:15 +0700

    todo api

--- routes/api_router.js
+++ routes/api_router.js
@@ -2,7 +2,7 @@ const express = require('express')
 const route = express.Router()
 const apiController = require('../routes/APi')
 
-
+// todo some api
 route.get('/get-all-list-room-finish',apiController.getAlllistromed)
 
 route.get('/get-all-list-account',apiController.getAllAccount)
