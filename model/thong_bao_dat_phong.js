76f60d203bf360afc6725bfb00b7f32102ca8d52
DESKTOP-3U1FHHU\Admin <hangocluc2001@gmail.com>
2021-12-04 23:16:38 +0700

    update thong bao

--- model/thong_bao_dat_phong.js
+++ model/thong_bao_dat_phong.js
@@ -6,17 +6,16 @@ const ThongBaoDatPhong = new Schema({
     sophong: String,
     hoten: String,
     sdt: String,
+    loaiPhong: String,
+    hangPhong:String,
     cccd: Number,
     email: String,
     ngaynhan: String,
     ngayTra: String,
     sodem: Number,
     soNguoi: Number,
-    datChoMinh: Boolean,
-    datChoNguoiKhac: Boolean,
     gioNhanPhong: String,
     gioTra: String,
     giaPhong: Number,
 })
-
 module.exports = mongoose.model('ThongBaoDatPhong', ThongBaoDatPhong)
