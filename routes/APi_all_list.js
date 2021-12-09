74f3335391909dacad0e1a9ac42a4adcfa137e43
DucTungLead <>
2021-12-09 11:34:58 +0700

    no message

--- routes/APi_all_list.js
+++ routes/APi_all_list.js
@@ -7,7 +7,9 @@ class APi_all_list {
 
 
     getAlllistromed(req, res, next) {
-        Datphongs.find({}).then(datphongs => res.json({
+        Datphongs.find({
+
+        }).then(datphongs => res.json({
             isSuccess: true,
             code: 200,
             message: "success",
@@ -37,8 +39,16 @@ class APi_all_list {
     getAllRooms(req, res, next) {
 
         Rooms.find({
-            wifi: req.query.wifi,
-            receptionist: req.query.receptionist,
+            wifi : req.query.wifi == null ? { $in: [ true, false ] } : req.query.wifi,
+            receptionist: req.query.receptionist == null ? { $in: [ true, false ] } : req.query.receptionist,
+            gym: req.query.gym == null ? { $in: [ true, false ] } : req.query.gym,
+            roomMeeting: req.query.roomMeeting == null ? { $in: [ true, false ] } : req.query.roomMeeting,
+            laundry: req.query.laundry == null ? { $in: [ true, false ] } : req.query.laundry,
+            pool: req.query.pool == null ? { $in: [ true, false ] } : req.query.pool,
+            restaurant: req.query.restaurant == null ? { $in: [ true, false ] } : req.query.restaurant,
+            elevator: req.query.elevator == null ? { $in: [ true, false ] } : req.query.elevator,
+            wheelChairWay: req.query.wheelChairWay == null ? { $in: [ true, false ] } : req.query.wheelChairWay,
+            shuttle: req.query.shuttle == null ? { $in: [ true, false ] } : req.query.shuttle,
         }).then(Rooms => res.json({
             isSuccess: true,
             code: 200,
